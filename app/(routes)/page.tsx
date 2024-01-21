32211cfcd4642a5944b6f76ef24068af18e6701b
rushi3691 <rushi3691@outlook.com>
2024-01-21 15:49:31 +0530

    Add force-dynamic option for home page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -2,8 +2,9 @@ import { Suspense } from "react";
 import ListSuspense from "./suspense";
 import ListVideos from "./listVideos";
 
-export default async function Home() {
+export const dynamic = "force-dynamic";
 
+export default async function Home() {
   return (
     // handle overflow in y axis
     <div className="ml-60 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 m-10 w-full h-full">
